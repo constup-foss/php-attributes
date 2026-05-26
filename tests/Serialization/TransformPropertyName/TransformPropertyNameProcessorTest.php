aa2cbf058e7e5c1a61cbdda72a19ce7d768e1057
constup <286199137+constup-foss@users.noreply.github.com>
2026-05-26 19:47:02 +0200

    2.0.0
    - `TransformPropertyNameProcessor` and `TransformPropertyValueProcessor` are refactored to work on reflection objects. Documentation is updated.

--- tests/Serialization/TransformPropertyName/TransformPropertyNameProcessorTest.php
+++ tests/Serialization/TransformPropertyName/TransformPropertyNameProcessorTest.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 namespace Constup\PhpAttributes\Tests\Serialization\TransformPropertyName;
 
 use Constup\PhpAttributes\Serialization\TransformPropertyName\TransformPropertyNameProcessor;
-use Constup\PhpAttributes\Tests\Serialization\TransformPropertyName\DataProvider\TransformPropertyNameProcessor\TransformDataProvider;
 use Constup\PhpAttributes\Tests\Serialization\TransformPropertyName\DataProvider\TransformPropertyNameProcessor\TransformReflectionPropertyDataProvider;
 use PHPUnit\Framework\Attributes\DataProviderExternal;
 use PHPUnit\Framework\TestCase;
@@ -13,48 +12,6 @@ use ReflectionProperty;
 
 class TransformPropertyNameProcessorTest extends TestCase
 {
-    #[DataProviderExternal(
-        TransformDataProvider::class,
-        'provide_HappyFlow'
-    )]
-    public function test_transform_HappyFlow(
-        string|object $classOrObject,
-        string $propertyName,
-        array $transformationArguments,
-        string $expected
-    ): void {
-        $result = TransformPropertyNameProcessor::transform(
-            $classOrObject,
-            $propertyName,
-            $transformationArguments,
-        );
-
-        $this->assertSame($expected, $result);
-    }
-
-    #[DataProviderExternal(
-        TransformDataProvider::class,
-        'provide_ErrorFlow'
-    )]
-    public function test_transform_ErrorFlow(
-        string|object $classOrObject,
-        string $propertyName,
-        array $transformationArguments,
-        string $expectedException,
-        ?int $expectedExceptionCode,
-    ): void {
-        $this->expectException($expectedException);
-        if ($expectedExceptionCode !== null) {
-            $this->expectExceptionCode($expectedExceptionCode);
-        }
-
-        TransformPropertyNameProcessor::transform(
-            $classOrObject,
-            $propertyName,
-            $transformationArguments,
-        );
-    }
-
     #[DataProviderExternal(
         TransformReflectionPropertyDataProvider::class,
         'provide_HappyFlow'
--- tests/Serialization/TransformPropertyValue/TransformPropertyValueProcessorTest.php
+++ tests/Serialization/TransformPropertyValue/TransformPropertyValueProcessorTest.php
@@ -5,8 +5,7 @@ declare(strict_types = 1);
 namespace Constup\PhpAttributes\Tests\Serialization\TransformPropertyValue;
 
 use Constup\PhpAttributes\Serialization\TransformPropertyValue\TransformPropertyValueProcessor;
-use Constup\PhpAttributes\Tests\Serialization\TransformPropertyValue\DataProvider\TransformPropertyValueProcessor\TransformDataProvider;
-use Constup\PhpAttributes\Tests\Serialization\TransformPropertyValue\DataProvider\TransformPropertyValueProcessor\TransformFromObjectDataProvider;
+use Constup\PhpAttributes\Tests\Serialization\TransformPropertyValue\DataProvider\TransformPropertyValueProcessor\TransformReflectionPropertyDataProvider;
 use PHPUnit\Framework\Attributes\DataProviderExternal;
 use PHPUnit\Framework\TestCase;
 use ReflectionProperty;
@@ -14,95 +13,7 @@ use ReflectionProperty;
 class TransformPropertyValueProcessorTest extends TestCase
 {
     #[DataProviderExternal(
-        TransformFromObjectDataProvider::class,
-        'provide_HappyFlow'
-    )]
-    public function test_transformFromObject_HappyFlow(
-        object $object,
-        string $propertyName,
-        array $transformationArguments,
-        mixed $expected
-    ): void {
-        $result = TransformPropertyValueProcessor::transformFromObject(
-            $object,
-            $propertyName,
-            $transformationArguments,
-        );
-
-        $this->assertSame($expected, $result);
-    }
-
-    #[DataProviderExternal(
-        TransformFromObjectDataProvider::class,
-        'provide_ErrorFlow'
-    )]
-    public function test_transformFromObject_ErrorFlow(
-        object $object,
-        string $propertyName,
-        array $transformationArguments,
-        string $expectedException,
-        ?int $expectedExceptionCode
-    ): void {
-        $this->expectException($expectedException);
-        if ($expectedExceptionCode !== null) {
-            $this->expectExceptionCode($expectedExceptionCode);
-        }
-
-        TransformPropertyValueProcessor::transformFromObject(
-            $object,
-            $propertyName,
-            $transformationArguments,
-        );
-    }
-
-    #[DataProviderExternal(
-        TransformDataProvider::class,
-        'provide_HappyFlow'
-    )]
-    public function test_transform_HappyFlow(
-        mixed $propertyValue,
-        string $className,
-        string $propertyName,
-        array $transformationArguments,
-        mixed $expected
-    ): void {
-        $result = TransformPropertyValueProcessor::transform(
-            $propertyValue,
-            $className,
-            $propertyName,
-            $transformationArguments,
-        );
-
-        $this->assertSame($expected, $result);
-    }
-
-    #[DataProviderExternal(
-        TransformDataProvider::class,
-        'provide_ErrorFlow'
-    )]
-    public function test_transform_ErrorFlow(
-        mixed $propertyValue,
-        string $className,
-        string $propertyName,
-        array $transformationArguments,
-        string $expectedException,
-        ?int $expectedExceptionCode
-    ): void {
-        $this->expectException($expectedException);
-        if ($expectedExceptionCode !== null) {
-            $this->expectExceptionCode($expectedExceptionCode);
-        }
-
-        TransformPropertyValueProcessor::transform(
-            $propertyValue,
-            $className,
-            $propertyName,
-            $transformationArguments,
-        );
-    }
-
-    #[DataProviderExternal(
-        TransformFromObjectDataProvider::class,
+        TransformReflectionPropertyDataProvider::class,
         'provide_HappyFlow'
     )]
     public function test_transformReflectionProperty_HappyFlow(
@@ -123,7 +34,7 @@ class TransformPropertyValueProcessorTest extends TestCase
     }
 
     #[DataProviderExternal(
-        TransformFromObjectDataProvider::class,
+        TransformReflectionPropertyDataProvider::class,
         'provide_ErrorFlow'
     )]
     public function test_transformReflectionProperty_ErrorFlow(
